c08ea84da71549880406c9dcfe5eedf2b3fe3341
Manu <codebymanuk@gmail.com>
2022-01-13 23:44:40 +0530

    js-basics

---
+++ js-basics/function.js
@@ -0,0 +1,9 @@
+console.log(beta()) //Is it work ? lets check it is working because call on below and call on this and no type error and no run time error 
+
+function alpha() {
+    return "A"
+}
+
+function beta() {
+    return "B"
+}
