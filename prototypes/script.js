108e2d5caed9ad8c496e68b42d6c5a411eab3535
Manu <codebymanuk@gmail.com>
2022-02-01 22:30:10 +0530

    prototypes

---
+++ prototypes/script.js
@@ -0,0 +1,19 @@
+let str = "Asdsad"
+let num = 233
+let bool = true
+let arr = [246, 436, 6, 346]
+let obj = { a: 10, b: 'asdasd' }
+let fun = function() {
+    console.log('yay!')
+}
+
+//if x and y are not primitive
+// x == y : true <- what does this mean?
+// this means they are the same object in memory
+
+console.log('typeof String', typeof String)
+console.log('typeof Boolean', typeof Boolean)
+console.log('typeof Number', typeof Number)
+console.log('typeof Array', typeof Array)
+console.log('typeof Object', typeof Object)
+console.log('typeof Function', typeof Function)
